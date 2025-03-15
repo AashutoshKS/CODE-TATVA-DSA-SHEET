77aafe50cb6133edae085a6a6d3b157edf12c2c6
AashutoshKS <aashutoshkumarsingh71@gmail.com>
2025-03-15 16:18:12 +0530

    Create 1827. Minimum Operations to Make the Array Increasing.cpp

---
+++ Array and Strings/1827. Minimum Operations to Make the Array Increasing.cpp
@@ -0,0 +1,34 @@
+
+//PROBLEM : 
+
+  You are given an integer array nums (0-indexed). In one operation, you can choose an element of the array and increment it by 1.
+  
+  For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+  Return the minimum number of operations needed to make nums strictly increasing.
+  
+  An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1. An array of length 1 is trivially strictly increasing.
+
+
+//APPROACH
+
+  For min number of operation  if arr[i] <= arr[i - 1].....the minimum operation to make arr[i-1] > arr[i] is to make arr[i-1] = arr[i] + 1
+  follow the same staps to calculate total number of operations
+
+TIME COMPLEXITY : O(n)
+SPACE COMPLEXITY : O(n)
+
+//code
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int count = 0;
+        for(int i = 1;i < nums.size(); i++) {
+            if(nums[i] <= nums[i-1]) {
+                count += nums[i-1] + 1 - nums[i];
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+        return count;
+    }
+};
