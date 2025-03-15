2f479bdf74daac1c4f715ae4cfdb1397a20d58d0
AashutoshKS <aashutoshkumarsingh71@gmail.com>
2025-03-15 17:00:30 +0530

    Create 73. Set Matrix Zeroes.cpp

---
+++ 2D ARRAy/73. Set Matrix Zeroes.cpp
@@ -0,0 +1,54 @@
+//PROBLEM : 
+    Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+    You must do it in place.
+
+
+//APPROACH : 
+
+    traverse the array and if any valu eis equal to 0 change the 0th row and 0th col value to 0...use that value to change the whole row and col to 0
+      ie. traverse again if the pertivular row or col is 0 change the element to 0
+
+
+//COMPLEXITY 
+      TIME COMPLEXITY  : O(2*n*n + 2*n) = O(n*n)
+      SPACE COMPLEXITY  : O(1)
+
+      
+
+//CODE
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0 = 1;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j != 0){
+                        matrix[0][j] = 0;
+                    } else{
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
+                if( matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int i = 0; i < matrix[0].size() ;i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if(col0 == 0){
+            for(int i = 0; i < matrix.size() ;i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
