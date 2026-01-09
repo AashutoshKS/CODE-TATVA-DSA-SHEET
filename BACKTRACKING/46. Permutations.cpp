4bd334499b5aa9379dc0a9e66da9e2e18b308c76
Aashutosh Kumar Singh <aashutoshkumarsingh71@gmail.com>
2026-01-09 17:57:24 +0530

    Create 46. Permutations.cpp

---
+++ BACKTRACKING/46. Permutations.cpp
@@ -0,0 +1,33 @@
+//PROBLEM :
+
+      Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+
+
+//CODE :
+      class Solution {
+        public:
+            vector<vector<int>> permute(vector<int>& nums) {
+                vector<vector<int>> ans;
+                dfs(nums, vector<bool>(nums.size()), {}, ans);
+                return ans;
+            }
+        
+        private:
+            void dfs(const vector<int>& nums, vector<bool>&& used, vector<int>&& path, vector<vector<int>>& ans){
+                if (path.size() == nums.size()) {
+                    ans.push_back(path);
+                    return;
+                }
+        
+                for (int i = 0; i < nums.size(); ++i) {
+                    if (used[i])
+                        continue;
+                    used[i] = true;
+                    path.push_back(nums[i]);
+                    dfs(nums, move(used), move(path), ans);
+                    path.pop_back();
+                    used[i] = false;
+                }
+            }
+        };
